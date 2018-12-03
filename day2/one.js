9313617efc80d7d64eb92105a995f70232131a34
Jane Kim <jane@janekim.me>
2018-12-03 17:52:16 -0500

    add linter

---
+++ day2/one.js
@@ -0,0 +1,11 @@
+const fs = require('fs')
+const input = fs.readFileSync('day2/input', { encoding: 'utf8'})
+const lines = input.split('\n')
+
+let answer
+for (let i = 0; i < lines.length; i++) {
+  const element = lines[i];
+}
+
+
+console.log('answer:', answer)
